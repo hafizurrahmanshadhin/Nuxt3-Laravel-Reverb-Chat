a04d437434ea8704f2d6bd12f6729089c7499a66
Harish Kumar <harish.pra22@gmail.com>
2024-10-03 16:38:18 +0530

    feat: add chat functionality

--- laravel-api/routes/api.php
+++ laravel-api/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\ChatMessage;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -15,3 +16,33 @@ Route::get('/users', function (Request $request) {
 Route::get('/users/{user}', function (User $user) {
     return $user;
 })->middleware('auth:sanctum');
+
+Route::get('/messages/{user}', function (User $user, Request $request) {
+    return ChatMessage::query()
+    ->where(function ($query) use ($user, $request) {
+        $query->where('sender_id', $request->user()->id)
+            ->where('receiver_id', $user->id);
+    })
+    ->orWhere(function ($query) use ($user, $request) {
+        $query->where('sender_id', $user->id)
+            ->where('receiver_id', $request->user()->id);
+    })
+   ->with(['sender', 'receiver'])
+   ->orderBy('id', 'asc')
+   ->get();
+})
+->middleware('auth:sanctum');
+
+Route::post('/messages/{user}', function (User $user, Request $request) {
+    $request->validate([
+        'message' => 'required|string'
+    ]);
+
+    $message = ChatMessage::create([
+        'sender_id' => $request->user()->id,
+        'receiver_id' => $user->id,
+        'text' => $request->message
+    ]);
+
+    return $message;
+});
